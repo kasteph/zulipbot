f6629c0bb4f4fb072602a7015a80193e58b6e810
Steph Samson <sdvsamson@gmail.com>
2014-11-06 20:00:48 -0500

    Turns out get_subscriptions isnt actually useful; added send_stream_message and send_private_message

--- zulipbot/bot.py
+++ zulipbot/bot.py
@@ -1,5 +1,6 @@
 import zulip
 import requests
+from requests.auth import HTTPBasicAuth
 
 
 class Bot(zulip.Client):
@@ -15,12 +16,12 @@ class Bot(zulip.Client):
         super(Bot, self).__init__(self._email, self._key)
 
     def _zulip_request(self, endpoint):
-        return requests.get(endpoint, auth=requests.HTTPBasicAuth(self._email, self._key))
+        '''A generic zulip request.'''
+        return requests.get(endpoint, auth=HTTPBasicAuth(self._email, self._key))
 
     def subscribe_to_all_streams(self):
         '''Subscribe to all Zulip streams.'''
         zulip_streams = self.zulip_api + '/streams'
-        print zulip_streams
         response = self._zulip_request(zulip_streams)
 
         if response.status_code == 200:
@@ -30,9 +31,17 @@ class Bot(zulip.Client):
         else:
             raise Exception(response)
 
-    def get_subscriptions(self):
-        '''Get list of joined streams.'''
-        joined_streams = self.zulip_api + '/users/me/subscriptions'
-        response = self._zulip_request(joined_streams)
+    def send_private_message(self, message_info, content):
+        self.send_message({
+            'type': 'private',
+            'to': message_info['sender_email'],
+            'content': content
+        })
 
-        return response
+    def send_stream_message(self, message_info, content):
+        self.send_message({
+            'type': 'stream',
+            'subject': message_info['subject'],
+            'to': message_info['display_recipient'],
+            'content': content
+        })
