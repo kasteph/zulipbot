dc2363c800ad9bb9947a61e2f7064f6c5c3de0ea
Steph Samson <sdvsamson@gmail.com>
2014-11-06 20:00:19 -0500

    Added comments and docstring

--- example.py
+++ example.py
@@ -1,39 +1,33 @@
 import os
 import zulipbot
 
+# I initialize the bot
 my_bot = zulipbot.Bot(os.environ['ZULIP_EMAIL'], os.environ['ZULIP_KEY'])
-print my_bot
-my_bot.send_message({
-        'type': 'stream',
-        'to': 'bot-test',
-        'subject': 'hello',
-        'content': 'I work!'
-    })
 
+# Then make it to subscribe to all the streams (this is optional)
 my_bot.subscribe_to_all_streams()
-print my_bot.get_subscriptions()
 
 
-def process_message(msg):
-    content = msg['content'].split()
-    sender_email = msg['sender_email']
+def process_message(message):
+    '''
+    process_message(message) -> <message> is a dictionary of meta-info
+    about the message(s) from streams or people that the bot listens to.
+
+    Where the main functionality of your bot goes. This function will be
+    passed as a callback function to Bot.call_on_each_message(<func>).
+    '''
+    content = message['content'].split()
+    sender_email = message['sender_email']
 
     if sender_email == os.environ['ZULIP_EMAIL']:
         return
 
     if content[0] == 'test':
-        if msg['type'] == 'stream':
-            my_bot.send_message({
-                'type': 'stream',
-                'subject': msg['subject'],
-                'to': msg['display_recipient'],
-                'content': 'YAY!'
-            })
+        if message['type'] == 'stream':
+            my_bot.send_stream_message(message, 'Stream message')
         else:
-            my_bot.send_message({
-                'type': 'private',
-                'to': msg['sender_email'],
-                'content': 'WOOT'
-            })
+            my_bot.send_private_message(message, 'Private message')
 
+
+# This runs forever!
 my_bot.call_on_each_message(process_message)
